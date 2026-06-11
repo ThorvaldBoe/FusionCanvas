45dd28b623adbebbfcca15842e7c87d1f9e39f26
ThorvaldBoe <thorvaldboe@gmail.com>
2026-06-11 12:09:45 +0200

    Added solution files

---
+++ src/FusionCanvas.App/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FusionCanvas.App.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
